aeea78e8bd468394d77e72a68191cb4e4884d4ca
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-08-18 18:42:54 +0200

    Consistent hash exchange

--- rabbitmq.producer2/src/main/java/com/course/rabbitmq/ProducerApplication.java
+++ rabbitmq.producer2/src/main/java/com/course/rabbitmq/ProducerApplication.java
@@ -1,8 +1,6 @@
 package com.course.rabbitmq;
 
-import com.course.rabbitmq.entity.InvoiceCancelledMessage;
 import com.course.rabbitmq.entity.InvoiceCreatedMessage;
-import com.course.rabbitmq.entity.InvoicePaidMessage;
 import com.course.rabbitmq.producer.InvoiceProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -30,18 +28,25 @@ public class ProducerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(100, 200);
-        var invoiceCreatedMessage = new InvoiceCreatedMessage(144.4, LocalDate.now(), "USD", randomInvoiceNumber);
-        invoiceProducer.sendInvoiceCreated(invoiceCreatedMessage);
+        for (int i = 0; i < 200; i++) {
+            var invoiceNumber = "INV-" + (i % 60);
+            var invoice = new InvoiceCreatedMessage(ThreadLocalRandom.current()
+                    .nextDouble(1, 200), LocalDate.now(), "USD", invoiceNumber);
+            invoiceProducer.sendInvoiceCreated(invoice);
+        }
 
-        randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(200, 300);
-        var randomPaymentNumber = "PAY-" + ThreadLocalRandom.current().nextInt(1000, 2000);
-        var invoicePaidMessage = new InvoicePaidMessage(randomInvoiceNumber, LocalDate.now(), randomPaymentNumber);
-        invoiceProducer.sendInvoicePaid(invoicePaidMessage);
-
-        randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(300, 400);
-        var invoiceCancelledMessage = new InvoiceCancelledMessage(randomInvoiceNumber, LocalDate.now(), "Invoice cancelled");
-        invoiceProducer.sendInvoiceCancelled(invoiceCancelledMessage);
+//        var randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(100, 200);
+//        var invoiceCreatedMessage = new InvoiceCreatedMessage(144.4, LocalDate.now(), "USD", randomInvoiceNumber);
+//        invoiceProducer.sendInvoiceCreated(invoiceCreatedMessage);
+//
+//        randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(200, 300);
+//        var randomPaymentNumber = "PAY-" + ThreadLocalRandom.current().nextInt(1000, 2000);
+//        var invoicePaidMessage = new InvoicePaidMessage(randomInvoiceNumber, LocalDate.now(), randomPaymentNumber);
+//        invoiceProducer.sendInvoicePaid(invoicePaidMessage);
+//
+//        randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(300, 400);
+//        var invoiceCancelledMessage = new InvoiceCancelledMessage(randomInvoiceNumber, LocalDate.now(), "Invoice cancelled");
+//        invoiceProducer.sendInvoiceCancelled(invoiceCancelledMessage);
 
 //        multiplePrefetchProducer.simulateTransaction();
 //        multiplePrefetchProducer.simulateScheduler();
