2bd3a036cf36f8d1af0de1a579aa81de3fd4131b
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-08-18 22:02:18 +0200

    Create queues and exchanges with java code in the consumer

--- rabbitmq.consumer2/src/main/java/com/course/rabbitmq/consumer/AnotherDummyConsumer.java
+++ rabbitmq.consumer2/src/main/java/com/course/rabbitmq/consumer/AnotherDummyConsumer.java
@@ -2,6 +2,10 @@ package com.course.rabbitmq.consumer;
 
 import com.course.rabbitmq.entity.DummyMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +13,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AnotherDummyConsumer {
 
-    @RabbitListener(queues = "q.dummy")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "q.auto-dummy", durable = "true"),
+            exchange = @Exchange(name = "x.auto-dummy", type = ExchangeTypes.DIRECT, durable = "true"),
+            key = "routingKey",
+            ignoreDeclarationExceptions = "true"
+    ))
     public void listenDummy(DummyMessage dummyMessage) {
         log.info("{}", dummyMessage);
     }
